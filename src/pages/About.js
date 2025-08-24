4b2f6fdb9a6fbb4780b70d8efc5e1e8c5f436fcd
gajaaddict <gaja89@gmail.com>
2025-08-24 22:51:36 +0530

    initial changes

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>We are a company committed to delivering quality products and services.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Products.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Products() {
+  return (
+    <div>
+      <h1>Our Products</h1>
+      <p>Here you can find the list of products we offer.</p>
+      <ul className="list-group">
+        <li className="list-group-item">Product 1</li>
+        <li className="list-group-item">Product 2</li>
+        <li className="list-group-item">Product 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Products;
