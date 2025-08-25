5121f1c5b55c4c0769824c46d58221c429ebad9b
gajaaddict <gaja89@gmail.com>
2025-08-25 12:51:43 +0530

    Hamburger menu

--- src/App.js
+++ src/App.js
@@ -7,16 +7,33 @@ import ContactUs from "./pages/ContactUs";
 function App() {
   return (
     <Router>
+
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
+          {/* Brand */}
           <Link className="navbar-brand" to="/">Dhatvibs</Link>
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
+          {/* Toggler (Hamburger) */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          {/* Collapsible Menu */}
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
               <li className="nav-item">
-                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/">About</Link>
               </li>
               {/* <li className="nav-item">
-                <Link className="nav-link" to="/products">Products</Link>
+                <Link className="nav-link" to="/about">About</Link>
               </li> */}
               <li className="nav-item">
                 <Link className="nav-link" to="/contact">Contact Us</Link>
